c34a8b5f73a343298e3bd0fc649544c427af062d
Asa Holland <hollandasa@gmail.com>
2020-06-14 18:28:29 -0700

    Attempted to add window frame

--- Calculator_GRE.py
+++ Calculator_GRE.py
@@ -6,14 +6,42 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.graphics import Color, Rectangle
+from kivy.graphics import Color, Rectangle, Line
 from kivy import Config
+from kivy.core.window import Window
+from kivy.lang import Builder
 
+Builder.load_string("""
+<BackgroundColor@Widget>
+	background_color: 1, 1, 1, 1
+	canvas.before:
+		Color:
+			rgba: root.background_color
+		Rectangle:
+			size: self.size
+			pos: self.pos
+
+<BackgroundLabel@Label+BackgroundColor>
+	background_color: 0, 0, 0, 0
+
+<TitleBar>
+	BoxLayout:
+		orientation: "horizontal"
+		BackgroundLabel:
+			text: "Calculator"
+			color: 0, 0, 0, 1
+			background_color: 0.82, 0.89, 0.93, 1
+			size_hint: None, None
+			height: self.texture_size[1]
+		Button:
+			text: "X"
+			size_hint: None, None
+			color: 0, 0, 0, 1
+			height: self.texture_size[1]
+			on_press: app.stop()
+""")
 
 # Set minimum size of the App window to match dimensions of the GRE calculator
-
-Config.set('graphics', 'width', '250')
-Config.set('graphics', 'height', '350')
 Config.set('kivy', 'window_icon', None)
 Config.set('graphics', 'resizable', False)
 
@@ -24,124 +52,149 @@ main_display_text = '0'
 
 
 class CalculatorDisplay(Label):
-        """Represents the display panel of the calculator. 
-        Inherits from Kivy Label class, and contains text that is set to align right."""
+	"""
+	Represents the display panel of the calculator.Inherits from Kivy Label class, and contains text that is set to align right.
+	"""
+
 	def on_size(self, *args):
 		self.canvas.before.clear()
 		with self.canvas.before:
 			Color(1, 1, 1, 1)
 			Rectangle(pos=self.pos, size=self.size, size_hint_y=None, text_size=self.setter('texture_size'),
-			          halign='right', valign='middle',  multiline=False,
-			          width=lambda *x: self.setter('text_size')(self, (self.width, None),
-		texture_size=lambda *x: self.setter('height')(self.canvas, self.texture_size[1])))
+					  halign='right', valign='middle', multiline=False,
+					  width=lambda *x: self.setter('text_size')(self, (self.width, None),
+																texture_size=lambda *x: self.setter('height')(
+																	self.canvas, self.texture_size[1])))
 
 
 class Button_With_Value(Button):
-        """Defines a button class that has a value, as well as a background color."""
+	"""
+	Defines a button class that has a value, as well as a background color.
+	"""
+
 	def on_size(self, button_value='', *args):
 		self.canvas.before.clear()
 		self.bind(size=self.setter('texture_size'))
 		with self.canvas.before:
 			Color(1, 1, 1, 1)
-			Rectangle(pos=self.pos, size=self.size, size_hint_y=None, size_hint_x=None, font=160,#text_size=(self.width, None),
-			          height=0.9*self.texture_size[1], width=0.5*self.texture_size[0])
+			Rectangle(pos=self.pos, size=self.size, size_hint_y=None, size_hint_x=None, font=160,
+					  # text_size=(self.width, None),
+					  height=0.9 * self.texture_size[1], width=0.5 * self.texture_size[0])
+
+		# Add black border around button
+		self.canvas.after.clear()
+		with self.canvas.after:
+			Color(0, 0, 0, 1)
+			self.line = Line(width=1.05,
+							 points=(self.x, self.y,
+									 self.x, self.y + self.height,
+									 self.x + self.width, self.y + self.height,
+									 self.x + self.width, self.y,
+									 self.x, self.y,), color=(0, 0, 0, 1))
 
 
 class CalculatorScreen(GridLayout):
-        """Defines a grid layout for the main buttons of the screen."
-	# Current equation is used for troubleshooting the display. This text shows a complete, un-evaluated equation. 
-        current_equation = '100'
-	
-        # Main Display is the evaluated value that the calculator prints on the screen.
-        main_display_text = '100'
-	
-        # The memory value is not displayed, but can be set and recalled.
-        memory = '0'
+	"""
+	Defines a grid layout for the main buttons of the screen.
+	"""
+	# Current equation is used for troubleshooting the display. This text shows a complete, un-evaluated equation.
+	current_equation = '100'
+
+	# Main Display is the evaluated value that the calculator prints on the screen.
+	main_display_text = '100'
+
+	# The memory value is not displayed, but can be set and recalled.
+	memory = '0'
 
 	def update(self):
-                """Updates the current main display and equation text values."""
+		"""
+		Updates the current main display and equation text values.
+		"""
 		self.main_display.text = main_display_text
 		self.equation_display.text = current_equation
 
 	def send_digit(self, sending_button):
-                """Sends the digit of the pressed button to the main display and the current equation."""
-		
-                # First, access the global variables for the main display and current equation
-                global main_display_text
+		"""
+		Sends the value of the button that has been pressed to the current equation and display.
+		:param sending_button:
+		:return:
+		"""
+
+		# First, access the global variables for the main display and current equation
+		global main_display_text
 		global current_equation
 
-                # If the main display is 0, clear the main display to avoid having numbers like 0123.5
+		# If the main display is 0, clear the main display to avoid having numbers like 0123.5
 		if main_display_text == '0':
 			main_display_text = ''
-		
-                # If the current equation is 0, clear the current equation to avoid evaluation errors.
-                if current_equation == '0':
+
+		# If the current equation is 0, clear the current equation to avoid evaluation errors.
+		if current_equation == '0':
 			current_equation = ''
-		
-                #  Clear any blank spaces in the main display, if present.
-                if ' ' in main_display_text:
+
+		#  Clear any blank spaces in the main display, if present.
+		if ' ' in main_display_text:
 			main_display_text = ''
 			current_equation = ''
-		
-                # Concatenate the text value from the button to the main display and equation values.
-                main_display_text += sending_button.text
+
+		# Concatenate the text value from the button to the main display and equation values.
+		main_display_text += sending_button.text
 		current_equation += sending_button.text
-		
-                # Then update the GUI representations of the main display and equation fields.
-                self.update()
+
+		# Then update the GUI representations of the main display and equation fields.
+		self.update()
 
 	def send_operator(self, sending_button):
-                """Function used for operator buttons (+, -, /, *, =) to send those values to the equation and main displays."""
-		
-                # Access the global variables
-                global main_display_text
+		"""Function used for operator buttons (+, -, /, *, =) to send those values to the equation and main displays."""
+
+		# Access the global variables
+		global main_display_text
 		global current_equation
-		
-                # If the main display equals zero, clear it.
-                if main_display_text == '0':
+
+		# If the main display equals zero, clear it.
+		if main_display_text == '0':
 			main_display_text = ''
-		
-                # If the current equation is empty, set it to zero to avoid evaluation errors.
-                if current_equation == '':
+
+		# If the current equation is empty, set it to zero to avoid evaluation errors.
+		if current_equation == '':
 			current_equation = '0'
-		
-                # Set the main display to zero, append the operator button value to the equation, and update.
-                # We use the button value here instead of the button text because of the symbols * and /, 
-                # which are not the symbols displayed as the keys on the calculator.
-                main_display_text = '0'
+
+		# Set the main display to zero, append the operator button value to the equation, and update.
+		# We use the button value here instead of the button text because of the symbols * and /,
+		# which are not the symbols displayed as the keys on the calculator.
+		main_display_text = '0'
 		current_equation += sending_button.button_value
 		self.update()
 
 	def send(self, sending_button):
-                """Generic function to append a button text value to the main display and equation."""
+		"""Generic function to append a button text value to the main display and equation."""
 		global main_display_text
 		global current_equation
-		
-                # If main display equals zero, clear it.
-                if main_display_text == '0':
+
+		# If main display equals zero, clear it.
+		if main_display_text == '0':
 			main_display_text = ''
-		
-                # If current equation equals zero, clear it
-                if current_equation == '0':
+
+		# If current equation equals zero, clear it
+		if current_equation == '0':
 			current_equation = ''
-		
-                # Concatenate the main display text and current equation text
-                # with the text value of the button pressed.
-                main_display_text += sending_button.text
+
+		# Concatenate the main display text and current equation text
+		# with the text value of the button pressed.
+		main_display_text += sending_button.text
 		current_equation += sending_button.text
 		self.update()
 
 	def evaluate(self, sending_button):
-                """Evaluates the current equation and updates the main display with the result."""
-		
-                # First, access the global variables for the main display and current equation.
-                global main_display_text
-		global current_equation
-		
+		"""Evaluates the current equation and updates the main display with the result."""
 
-                try:
+		# First, access the global variables for the main display and current equation.
+		global main_display_text
+		global current_equation
+
+		try:
 			# Evaluate the current equation, round it to seven places, and set that value as the total.
-                        total = round(eval(current_equation), 7)
+			total = round(eval(current_equation), 7)
 			if abs(total) > 10000000:
 				main_display_text = 'GRE Error'
 				current_equation = ''
@@ -257,13 +310,14 @@ class CalculatorScreen(GridLayout):
 		self.update()
 
 	def __init__(self, **kwargs):
+		# global kv_string
 		global current_equation
 		global main_display_text
+
 		# initial set up of vertical screen setup
 		super(CalculatorScreen, self).__init__(**kwargs)
 		self.orientation = 'vertical'
 		self.cols = 1
-
 
 		# create title label at top of screen
 		# self.main_label = Label(text='\n[b]Work in progress[/b]\n', markup=True)
@@ -278,8 +332,16 @@ class CalculatorScreen(GridLayout):
 		# self.equation_display.size_hint_y = None
 		# self.equation_display.color = (1, 0, 0, 1)
 		# self.equation_display.font_size = 30
+		#
 
-		# Add Box Layout for display
+		class TitleBar(BoxLayout):
+			pass
+
+		# Add Title Bar
+		self.title_bar = TitleBar(size_hint=(None, None))
+		self.add_widget(self.title_bar)
+
+		#  Add Box Layout for display
 		self.box_display = BoxLayout(orientation='horizontal')
 		self.add_widget(self.box_display)
 		self.cols = 1
@@ -301,7 +363,6 @@ class CalculatorScreen(GridLayout):
 		self.main_display.size_hint_y = None
 		self.main_display.color = (0, 0, 0, 1)
 		self.box_display.add_widget(self.main_display)
-
 
 		# create horizontal box for two side by side buttons
 		self.buttonGrid = GridLayout()
@@ -331,7 +392,7 @@ class CalculatorScreen(GridLayout):
 			(18, '-', True, '-', self.send_operator),
 			(19, '\u221A', False, 'NA', self.root),
 			(20, '\u00B1', False, 'NA', self.plus_minus),
-			(21,  '0', True, 0, self.send_digit),
+			(21, '0', True, 0, self.send_digit),
 			(22, '.', True, '.', self.send),
 			(23, '+', True, '+', self.send_operator),
 			(24, '=', False, 'NA', self.evaluate),
@@ -339,7 +400,8 @@ class CalculatorScreen(GridLayout):
 
 		for button in tuple_of_buttons:
 			name = 'button_' + str(button[0])
-			self.name = Button_With_Value(text=button[1], disabled=False, color=(0, 0, 0, 1),)
+			self.name = Button_With_Value(text=button[1], disabled=False, color=(0, 0, 0, 1),
+										  background_color=(1, 1, 1, 1))
 			self.buttonGrid.add_widget(self.name)
 			self.name.button_value = button[3]
 			# self.name.bind(texture_size=self.name.setter('size'))
@@ -354,6 +416,8 @@ class CalculatorScreen(GridLayout):
 class MyApp(App):
 	def build(self):
 		self.title = 'Calculator'
+		Window.size = (300, 430)
+		Window.borderless = True
 		return CalculatorScreen()
 
 
